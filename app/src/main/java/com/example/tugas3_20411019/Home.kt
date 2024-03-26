d5158b61b6dcded4c8d5b940e32d29e1f0ae7e53
elia itlay <itlayelia@gmail.com>
2024-03-26 21:36:33 +0900

    Initial commit

--- app/src/main/java/com/example/tugas3_20411019/Home.kt
+++ app/src/main/java/com/example/tugas3_20411019/Home.kt
@@ -1,23 +1,32 @@
-@file:Suppress("ClassName")
-
 package com.example.tugas3_20411019
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Register_Activity : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.button)) { v, insets ->
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnShare)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        //Implisit Intent
+        val url ="https://github.com/eliaitlay"
+        val implicitBtnShare =findViewById<Button>(R.id.btnShare)
+
+        implicitBtnShare.setOnClickListener{
+            val implicitIntentnt=Intent(Intent.ACTION_VIEW,Uri.parse(url))
+            startActivity(implicitIntentnt)
         }
     }
+}
